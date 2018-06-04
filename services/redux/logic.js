6fbffb95c77e9bf4e97830e3b8ebf3b27fa90604
Andrii <andrii@fream.pl>
2018-06-04 13:43:47 +0200

    import all logic in one place

--- services/redux/logic.js
+++ services/redux/logic.js
@@ -1,7 +1,9 @@
-// import { logic as configLogic } from '../../redux/config';
-// import { logic as viewLogic } from '../../redux/view';
+import { logic as configLogic } from '../../redux/config';
+import { logic as viewLogic } from '../../redux/view';
+import { logic as moviesLogic } from '../../redux/movies';
 
 export default [
-  // ...configLogic,
-  // ...viewLogic,
+  ...configLogic,
+  ...viewLogic,
+  ...moviesLogic,
 ];

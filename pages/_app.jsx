2f62bd6701333f6f13a42fabbfb0705a925a1e4b
Andrii <andrii@fream.pl>
2018-09-21 14:35:09 +0200

    add getInitialProps hook to _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -24,6 +24,18 @@ Router.onRouteChangeError = () => {
 
 
 class MyApp extends App {
+  static async getInitialProps({ Component, /* router, */ ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return {
+      pageProps,
+    };
+  }
+
   constructor(props, context) {
     super(props, context);
     this.pageContext = this.props.pageContext || getPageContext();

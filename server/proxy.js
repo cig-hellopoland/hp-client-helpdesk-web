a2b9f28514522c600e2dc3e90085509d0a7a5034
Andrii <andrii@fream.pl>
2018-09-21 16:08:26 +0200

    add server-mock script to package.json

---
+++ server/proxy.js
@@ -0,0 +1,18 @@
+const proxyMiddleware = require('http-proxy-middleware');
+const config = require('../config/develop.config');
+
+const port = parseInt(process.env.NODE_PORT, 10) || 3000;
+const serverConfig = config.server;
+const baseURL = serverConfig.apiURL || `http://localhost:${port}/api`;
+
+const proxySettings = {
+  '/api': {
+    target: baseURL,
+    pathRewrite: { '^/api': '/' },
+    changeOrigin: true,
+  },
+};
+
+const middleware = Object.entries(proxySettings).map(entry => proxyMiddleware(entry[0], entry[1]));
+
+module.exports = middleware;

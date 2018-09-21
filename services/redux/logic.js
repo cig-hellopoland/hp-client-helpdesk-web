092632515adaacda75b78939b9fed3d06f37d81d
Andrii <andrii@fream.pl>
2018-09-21 14:08:58 +0200

    remove config duck

--- services/redux/logic.js
+++ services/redux/logic.js
@@ -1,9 +1,7 @@
-import { logic as configLogic } from 'redux/config';
 import { logic as viewLogic } from 'redux/view';
 import { logic as moviesLogic } from 'redux/movies';
 
 export default [
-  ...configLogic,
   ...viewLogic,
   ...moviesLogic,
 ];

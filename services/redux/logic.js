cf8df1e80a31a8a9ca80328768662d368171234b
Janusz Przybylski <janusz@fream.pl>
2019-02-11 17:43:59 +0100

    HPL-621 Added SalexExport component

--- services/redux/logic.js
+++ services/redux/logic.js
@@ -1,8 +1,6 @@
 import parseReduxLogic from 'utils/parseReduxLogic';
-import { logic as moviesLogic } from 'redux/movies';
 import { logic as profileLogic } from 'redux/profile';
 
 export default parseReduxLogic({
-  moviesLogic,
   profileLogic,
 });

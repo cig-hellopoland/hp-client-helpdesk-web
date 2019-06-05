04647d2c8915771e7c2b0d4945ed7e45f91602e2
btuchowski <btuchowski@fream.pl>
2019-06-05 15:09:17 +0200

    Add: Open Dialog when form successed

--- views/Partners/PartnerCreate.jsx
+++ views/Partners/PartnerCreate.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import withAuth from 'services/auth/withAuth';
@@ -6,6 +6,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Layout from 'components/Layout';
 import AddPartnerForm from './components/PartnerForm';
+import SuccessDialog from './components/SuccessDialog';
 
 const styles = theme => ({
   root: {
@@ -14,13 +15,44 @@ const styles = theme => ({
   },
 });
 
-const PartnerCreate = ({ classes }) => (
-  <Layout>
-    <Paper className={classes.root}>
-      <AddPartnerForm />
-    </Paper>
-  </Layout>
-);
+class PartnerCreate extends Component {
+  constructor() {
+    super();
+    this.state = {
+      confirmationDialog: true,
+    };
+  }
+
+  handleSubmitSuccess = (sightId, actions) => {
+    const { resetForm, setSubmitting } = actions;
+
+    setSubmitting(false);
+    resetForm();
+    this.handleSuccessDialogOpen();
+  }
+
+  handleSuccessDialogOpen = () => this.setState({ confirmationDialog: true });
+
+  handleSuccessDialogClose = () => this.setState({ confirmationDialog: false });
+
+  render() {
+    const { classes } = this.props;
+    const { confirmationDialog } = this.state;
+    return (
+      <Fragment>
+        <Layout>
+          <Paper className={classes.root}>
+            <AddPartnerForm
+              onSubmitSuccess={this.handleSubmitSuccess}
+            />
+          </Paper>
+        </Layout>
+        <SuccessDialog open={confirmationDialog} onClose={this.handleSuccessDialogClose} />
+      </Fragment>
+    );
+  }
+}
+
 
 PartnerCreate.propTypes = {
   classes: PropTypes.shape({}).isRequired,

4296256d72d01e24c0cfb266391ef5c276bd622b
Janusz Przybylski <janusz@fream.pl>
2019-09-04 08:35:00 +0200

    HPL-812 Updated main menu

--- components/Layout/MenuDrawer.jsx
+++ components/Layout/MenuDrawer.jsx
@@ -37,7 +37,7 @@ const MenuDrawer = ({
           disabled, label, href, Icon,
         }) => (href
           ? (
-            <Link key={label} href={href} passHref>
+            <Link key={`${label}-${href}`} href={href} passHref>
               <ListItem button disabled={disabled} selected={currentPath === href}>
                 <ListItemIcon><Icon /></ListItemIcon>
                 <ListItemText primary={label} />
@@ -45,7 +45,7 @@ const MenuDrawer = ({
             </Link>
           )
           : (
-            <ListSubheader>{label}</ListSubheader>
+            <ListSubheader key={label}>{label}</ListSubheader>
           )))}
       </List>
     </nav>
@@ -57,8 +57,8 @@ MenuDrawer.propTypes = {
   currentPath: PropTypes.string.isRequired,
   menuItems: PropTypes.arrayOf(PropTypes.shape({
     label: PropTypes.string.isRequired,
-    href: PropTypes.string.isRequired,
-    Icon: PropTypes.func.isRequired,
+    href: PropTypes.string,
+    Icon: PropTypes.func,
   })).isRequired,
 };
 

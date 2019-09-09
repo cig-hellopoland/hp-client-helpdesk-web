27794a233635518ff4ac9f607edda6bd108a5a63
Janusz Przybylski <janusz@fream.pl>
2019-09-09 09:25:33 +0200

    HPL-820 Basic SightEvent edit view

---
+++ views/Market/SightEvents/SightEventEdit.jsx
@@ -0,0 +1,303 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import _sortedUniq from 'lodash/sortedUniq';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Snackbar from '@material-ui/core/Snackbar';
+import Typography from '@material-ui/core/Typography';
+import { withRouter } from 'next/router';
+import {
+  actions as categoriesActions,
+  selectors as categoriesSelectors,
+} from 'redux/categories';
+import withAuth from 'services/auth/withAuth';
+import { CONTENT_LANGUAGES, DEFAULT_LANGUAGE } from 'utils/translations';
+import Layout from 'components/Layout';
+import ContentTranslation from 'components/ContentTranslation';
+import SightEventForm from './components/SightEventForm';
+
+const styles = theme => ({
+  root: {
+    flex: 1,
+    padding: theme.spacing.unit * 2,
+  },
+});
+
+class SightEventEdit extends React.Component {
+  state = {
+    availableTranslations: CONTENT_LANGUAGES,
+    selectedTranslation: DEFAULT_LANGUAGE,
+    snackbarOpen: false,
+    snackbarMessage: '',
+  };
+
+  componentDidMount() {
+    const {
+      itemId, clearError, clearItem, error, item,
+    } = this.props;
+
+    if (item) {
+      clearItem();
+    }
+
+    if (error) {
+      clearError();
+    }
+
+    if (itemId) {
+      this.handleFetchItem(itemId, DEFAULT_LANGUAGE);
+    }
+  }
+
+  getFormValues = (item) => {
+    const { itemId } = this.props;
+    const { selectedTranslation } = this.state;
+
+    if (item && itemId === item.id) {
+      const { availableLanguageVersions } = item;
+      const hasNewTranslation = !availableLanguageVersions.some(lng => lng === selectedTranslation);
+
+      if (hasNewTranslation) {
+        const { label, ...itemProps } = item;
+
+        return { ...itemProps };
+      }
+
+      return { ...item };
+    }
+
+    return null;
+  };
+
+  handleFetchItemFailure = () => {
+    const { clearError, error } = this.props;
+
+    this.handleSnackbarOpen(error && error.message);
+
+    if (clearError) {
+      clearError();
+    }
+  };
+
+  handleFetchItemSuccess = () => {
+    const { item } = this.props;
+    const { availableLanguageVersions, language } = item || {};
+
+    this.setState({
+      availableTranslations: availableLanguageVersions,
+      selectedTranslation: language || DEFAULT_LANGUAGE,
+    });
+  };
+
+  handleFetchItem = (itemId, language) => {
+    const { fetchItem } = this.props;
+
+    fetchItem({
+      id: itemId,
+      options: {
+        headers: {
+          'Content-Language': language,
+        },
+      },
+      onFailure: this.handleFetchItemFailure,
+      onSuccess: this.handleFetchItemSuccess,
+    });
+  };
+
+  handleTranslationChange = (event) => {
+    const { item } = this.props;
+    const selectedTranslation = event.target.value;
+
+    this.setState({ selectedTranslation });
+
+    if (item && item.id) {
+      this.handleFetchItem(item.id, selectedTranslation);
+    }
+  };
+
+  handleTranslationCreate = (language) => {
+    this.setState(state => ({
+      availableTranslations: _sortedUniq([
+        ...state.availableTranslations,
+        language,
+      ]),
+      selectedTranslation: language,
+    }));
+  };
+
+  handleTranslationDefaultChangeFailure = () => {
+    const { clearError, error } = this.props;
+
+    this.handleSnackbarOpen(error && error.message);
+
+    if (clearError) {
+      clearError();
+    }
+  };
+
+  handleTranslationDefaultChangeSuccess = () => {
+    const { selectedTranslation } = this.state;
+    const { itemId } = this.props;
+
+    this.handleFetchItem(itemId, selectedTranslation);
+  };
+
+  handleTranslationDefaultChange = () => {
+    const { selectedTranslation } = this.state;
+    const { itemId, changeDefaultTranslation } = this.props;
+    const options = {
+      headers: {
+        'Content-Language': selectedTranslation,
+      },
+    };
+
+    changeDefaultTranslation({
+      id: itemId,
+      options,
+      onFailure: this.handleTranslationDefaultChangeFailure,
+      onSuccess: this.handleTranslationDefaultChangeSuccess,
+    });
+  };
+
+  handleTranslationDeleteFailure = () => {
+    const { clearError, error } = this.props;
+
+    this.handleSnackbarOpen(error && error.message);
+
+    if (clearError) {
+      clearError();
+    }
+  };
+
+  handleTranslationDeleteSuccess = () => {
+    const { itemId, item } = this.props;
+    const { defaultLanguage } = item || {};
+
+    this.handleFetchItem(itemId, defaultLanguage);
+  };
+
+  handleTranslationDelete = () => {
+    const { selectedTranslation } = this.state;
+    const { itemId, deleteTranslation } = this.props;
+
+    deleteTranslation({
+      id: itemId,
+      onFailure: this.handleTranslationDeleteFailure,
+      onSuccess: this.handleTranslationDeleteSuccess,
+      pathParams: {
+        languageVersion: selectedTranslation,
+      },
+    });
+  };
+
+  handleSnackbarOpen = message => this.setState({
+    snackbarOpen: true,
+    snackbarMessage: typeof message === 'string' ? message : 'Wystąpił nieznany błąd.',
+  });
+
+  handleSnackbarClose = () => this.setState({
+    snackbarOpen: false,
+    snackbarMessage: '',
+  });
+
+  handleSubmitSuccess = () => {
+    const { router } = this.props;
+
+    router.push('/market/categories');
+  };
+
+  render() {
+    const {
+      availableTranslations, selectedTranslation, snackbarOpen, snackbarMessage,
+    } = this.state;
+    const { classes, item, itemId } = this.props;
+    const { defaultLanguage } = item || {};
+
+    const pageTitle = itemId ? 'Edycja oferty' : 'Nowa oferta';
+    const hasLanguageActions = !!(item && item.id);
+
+    return (
+      <Layout>
+        <Paper className={classes.root}>
+          <Typography variant="h6">{pageTitle}</Typography>
+          <ContentTranslation
+            actions={hasLanguageActions}
+            TranslationPickerProps={{
+              defaultValue: defaultLanguage || DEFAULT_LANGUAGE,
+              items: availableTranslations || CONTENT_LANGUAGES,
+              onChange: this.handleTranslationChange,
+              value: selectedTranslation || DEFAULT_LANGUAGE,
+            }}
+            TranslationActionsProps={{
+              availableTranslations,
+              defaultTranslation: defaultLanguage,
+              selectedTranslation,
+              onCreateTranslation: this.handleTranslationCreate,
+              onDeleteTranslation: this.handleTranslationDelete,
+              onSetDefaultTranslation: this.handleTranslationDefaultChange,
+            }}
+          />
+          <br />
+          <SightEventForm
+            initialValues={this.getFormValues(item)}
+            language={selectedTranslation}
+            onSubmitSuccess={this.handleSubmitSuccess}
+          />
+          <Snackbar
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            open={snackbarOpen}
+            onClose={this.handleSnackbarClose}
+            ContentProps={{
+              'aria-describedby': 'message-id',
+            }}
+            message={snackbarMessage}
+          />
+        </Paper>
+      </Layout>
+    );
+  }
+}
+
+SightEventEdit.propTypes = {
+  itemId: PropTypes.number,
+  changeDefaultTranslation: PropTypes.func.isRequired,
+  classes: PropTypes.shape({}).isRequired,
+  clearError: PropTypes.func.isRequired,
+  clearItem: PropTypes.func.isRequired,
+  deleteTranslation: PropTypes.func.isRequired,
+  error: PropTypes.shape({}),
+  fetchItem: PropTypes.func.isRequired,
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    label: PropTypes.string,
+  }),
+  router: PropTypes.shape({}).isRequired,
+};
+
+SightEventEdit.defaultProps = {
+  itemId: null,
+  error: null,
+  item: null,
+};
+
+const mapStateToProps = state => ({
+  error: categoriesSelectors.getError(state),
+  item: categoriesSelectors.getItem(state),
+});
+
+const mapDispatchToProps = {
+  changeDefaultTranslation: categoriesActions.changeDefaultTranslation,
+  clearError: categoriesActions.clearError,
+  clearItem: categoriesActions.clearItem,
+  deleteTranslation: categoriesActions.deleteTranslation,
+  fetchItem: categoriesActions.fetchItem,
+};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withAuth(),
+  withRouter,
+  withStyles(styles),
+)(SightEventEdit);
---
+++ views/Market/SightEvents/components/SightEventForm/SightEventForm.jsx
@@ -0,0 +1,361 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import _isEqual from 'lodash/isEqual';
+import _isNumber from 'lodash/isNumber';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+import yupBoolean from 'yup/lib/boolean';
+import yupObject from 'yup/lib/object';
+import yupString from 'yup/lib/string';
+import {
+  actions as sightEventsActions,
+  selectors as sightEventsSelectors,
+} from '@hello-poland/commons/redux/sightEvents';
+import { DEFAULT_LANGUAGE } from 'utils/translations';
+import GridItem from 'components/GridItem';
+
+const commonProps = {
+  fullWidth: true,
+};
+
+// TODO: remove this function and change Switch implementation after it's fixed.
+// TODO: see https://github.com/stackworx/formik-material-ui/pull/42
+const fieldToSwitch = ({
+  field,
+  form: { isSubmitting },
+  disabled = false,
+  ...props
+}) => ({
+  disabled: isSubmitting || disabled,
+  ...props,
+  ...field,
+  value: field.name,
+  checked: field.value,
+});
+
+const styles = {
+  errorIcon: {
+    fontSize: 48,
+  },
+  formControl: {
+    width: '100%',
+  },
+  section: {
+    marginTop: 40,
+  },
+};
+
+class SightEventForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { initialValues } = this.props;
+
+    this.state = {
+      initialValues: this.getInitialValues(initialValues || {}),
+    };
+
+    // TODO: nested validation seems not working
+    // TODO: see https://github.com/jaredpalmer/formik/issues/986
+    this.validationSchema = yupObject().shape({
+      name: yupString().min(3).max(250).required(),
+      published: yupBoolean(),
+      // generalAdmission: yupBoolen(),
+      lead: yupString().min(10).max(250),
+      description: yupString().min(10).max(2500).required(),
+      email: yupString().email().trim(),
+      phone: yupString().min(9).trim(),
+      // location: yupObject().shape({
+      //   directions: yupString().min(5).max(255),
+      //   street: yupString().min(5),
+      //   zipCode: yupString().min(6).max(6),
+      //   city: yupString().min(3),
+      //   country: yupString().min(5),
+      // }),
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { initialValues: prevInitialValues } = prevProps;
+    const { initialValues } = this.props;
+
+    if (!_isEqual(prevInitialValues, initialValues)) {
+      this.setInitialValues(initialValues);
+    }
+  }
+
+  getInitialValues = (initialValues) => {
+    const { location: initialLocation, pdfAttachment: files, ...details } = initialValues || {};
+    const location = initialLocation || {};
+
+    return {
+      id: details.id || '',
+      sightId: details.sightId || '',
+      name: details.name || '',
+      published: details.published || false,
+      lead: details.lead || '',
+      description: details.description || '',
+      email: details.email || '',
+      phone: details.phone || '',
+      location: {
+        directions: location.directions || '',
+        street: location.street || '',
+        zipCode: location.zipCode || '',
+        city: location.city || '',
+        country: location.country || '',
+      },
+    };
+  };
+
+
+  setInitialValues = initialValues => this.setState({
+    initialValues: this.getInitialValues(initialValues || {}),
+  });
+
+  handleSubmit = (values, actions) => {
+    const { initialValues, language, onSubmit } = this.props;
+
+    if (onSubmit) {
+      onSubmit(values, actions);
+
+      return;
+    }
+
+    const { createItem, createTranslation, updateItem } = this.props;
+    const { id, ...data } = values;
+
+    const payload = {
+      data,
+      onFailure: this.handleSubmitFailure(actions),
+      onSuccess: this.handleSubmitSuccess(actions),
+      options: {
+        headers: {
+          'Content-Language': language,
+        },
+      },
+    };
+
+    let submitAction = createItem;
+
+    if (_isNumber(id)) {
+      payload.data.id = id;
+
+      if (initialValues.language === language) {
+        submitAction = updateItem;
+        payload.id = id;
+        payload.pathParams = {
+          languageVersion: language,
+        };
+      } else {
+        submitAction = createTranslation;
+      }
+    }
+
+    submitAction(payload);
+  };
+
+  handleSubmitFailure = actions => () => {
+    const { onSubmitFailure } = this.props;
+
+    if (onSubmitFailure) {
+      onSubmitFailure(actions);
+    }
+
+    const { setSubmitting } = actions;
+
+    setSubmitting(false);
+  };
+
+  handleSubmitSuccess = actions => (sightId) => {
+    const { onSubmitSuccess, clearError } = this.props;
+
+    if (onSubmitSuccess) {
+      onSubmitSuccess(sightId, actions);
+
+      return;
+    }
+
+    const { resetForm, setSubmitting } = actions;
+
+    clearError();
+    setSubmitting(false);
+    resetForm();
+  };
+
+  render() {
+    const {
+      classes, FormikProps, hideButtons, hideErrors, initialValues: itemValues, language,
+      requestError,
+    } = this.props;
+    const { initialValues } = this.state;
+    const { data: errorData } = requestError || {};
+    const { message: errorMessage } = errorData || {};
+    const { defaultLanguage, id: itemId } = itemValues || {};
+    const isDefaultTranslation = itemId && defaultLanguage !== language;
+
+    return (
+      <Formik
+        enableReinitialize
+        {...FormikProps}
+        initialValues={initialValues}
+        validationSchema={this.validationSchema}
+        onSubmit={this.handleSubmit}
+      >
+        {({ isSubmitting } = {}) => (
+          <Form autoComplete="off" noValidate>
+            <Grid container spacing={16}>
+              <GridItem>
+                <Typography variant="h6">Dane podstawowe</Typography>
+              </GridItem>
+              <Hidden xsUp>
+                <GridItem>
+                  <Field name="id" hidden component={TextField} {...commonProps} />
+                </GridItem>
+              </Hidden>
+              <Hidden xsUp>
+                <GridItem>
+                  <Field name="sightId" hidden component={TextField} {...commonProps} />
+                </GridItem>
+              </Hidden>
+              <GridItem>
+                <Field name="name" label="Nazwa oferty" required component={TextField} {...commonProps} />
+              </GridItem>
+              {isDefaultTranslation
+                && (
+                  <GridItem md={4} sm={4}>
+                    <Field
+                      name="published"
+                      render={switchProps => (
+                        <FormControlLabel
+                          control={<Switch {...fieldToSwitch(switchProps)} />}
+                          label="Publikuj"
+                        />
+                      )}
+                    />
+                  </GridItem>
+                )
+              }
+              <GridItem>
+                <Field name="lead" label="Wprowadzenie" component={TextField} {...commonProps} />
+              </GridItem>
+              <GridItem>
+                <Field name="description" label="Opis oferty" required component={TextField} {...commonProps} multiline rowsMax={20} />
+              </GridItem>
+              {isDefaultTranslation
+                && (
+                  <React.Fragment>
+                    <GridItem>
+                      <Typography variant="h6" className={classes.title}>Dane kontaktowe</Typography>
+                    </GridItem>
+                    <GridItem>
+                      <Field name="email" label="Adres e-mail" type="email" component={TextField} {...commonProps} />
+                    </GridItem>
+                    <GridItem>
+                      <Field name="phone" label="Numer telefonu" component={TextField} {...commonProps} />
+                    </GridItem>
+                    <GridItem>
+                      <Typography variant="h6" className={classes.title}>Lokalizacja</Typography>
+                    </GridItem>
+                    <GridItem>
+                      <Field name="location.street" label="Ulica" component={TextField} {...commonProps} />
+                    </GridItem>
+                    <GridItem md={4} sm={4}>
+                      <Field name="location.zipCode" label="Kod pocztowy" component={TextField} {...commonProps} />
+                    </GridItem>
+                    <GridItem md={8} sm={8}>
+                      <Field name="location.city" label="Miasto" component={TextField} {...commonProps} />
+                    </GridItem>
+                    <GridItem>
+                      <Field name="location.country" label="Kraj" component={TextField} {...commonProps} />
+                    </GridItem>
+                    <GridItem>
+                      <Field name="location.directions" label="Wskazówki dojazdu" component={TextField} {...commonProps} multiline rowsMax={20} />
+                    </GridItem>
+                  </React.Fragment>
+                )
+              }
+            </Grid>
+            {(!hideButtons || (!hideErrors && errorMessage))
+              && (
+                <Grid container spacing={16} justify="flex-end" className={classes.section}>
+                  {!hideErrors && errorMessage
+                    && (
+                      <GridItem container md={9} sm={9}>
+                        <Typography color="error">{errorMessage}</Typography>
+                      </GridItem>
+                    )
+                  }
+                  {!hideButtons
+                    && (
+                      <GridItem container md={3} sm={3} justify="flex-end">
+                        <Button variant="contained" color="primary" type="submit" disabled={isSubmitting}>
+                          Zapisz
+                        </Button>
+                      </GridItem>
+                    )
+                  }
+                </Grid>
+              )
+            }
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+}
+
+SightEventForm.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+  clearError: PropTypes.func.isRequired,
+  createItem: PropTypes.func.isRequired,
+  createTranslation: PropTypes.func.isRequired,
+  FormikProps: PropTypes.shape({}),
+  hideButtons: PropTypes.bool,
+  hideErrors: PropTypes.bool,
+  initialValues: PropTypes.shape({}),
+  language: PropTypes.string,
+  onSubmit: PropTypes.func,
+  onSubmitFailure: PropTypes.func,
+  onSubmitSuccess: PropTypes.func,
+  requestError: PropTypes.shape({
+    message: PropTypes.string,
+  }),
+  updateItem: PropTypes.func.isRequired,
+};
+
+SightEventForm.defaultProps = {
+  FormikProps: null,
+  hideButtons: false,
+  hideErrors: false,
+  initialValues: null,
+  language: DEFAULT_LANGUAGE,
+  onSubmit: null,
+  onSubmitFailure: null,
+  onSubmitSuccess: null,
+  requestError: null,
+};
+
+const mapStateToProps = state => ({
+  requestError: sightEventsSelectors.getError(state),
+});
+
+const mapDispatchToProps = {
+  clearError: sightEventsActions.clearError,
+  createItem: sightEventsActions.createItem,
+  createTranslation: sightEventsActions.createTranslation,
+  updateItem: sightEventsActions.updateItem,
+};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withStyles(styles),
+)(SightEventForm);

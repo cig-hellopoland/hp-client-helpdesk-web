a33b0847b0e9409f1d6eb087c5087669fbc1eb51
Janusz Przybylski <janusz@fream.pl>
2019-09-10 11:59:25 +0200

    HPL-820 Fixed categories imports

--- services/redux/logic.js
+++ services/redux/logic.js
@@ -1,5 +1,6 @@
 import parseReduxLogic from 'utils/parseReduxLogic';
 import { logic as bookingLogic } from '@hello-poland/commons/redux/bookings';
+import { logic as categoriesLogic } from '@hello-poland/commons/redux/categories';
 import { logic as partnersLogic } from 'redux/partners';
 import { logic as profileLogic } from 'redux/profile';
 import { logic as sightEventsLogic } from '@hello-poland/commons/redux/sightEvents';
@@ -8,6 +9,7 @@ import { logic as ticketDefinitions } from '@hello-poland/commons/redux/ticketDe
 
 export default parseReduxLogic({
   bookingLogic,
+  categoriesLogic,
   partnersLogic,
   profileLogic,
   sightEventsLogic,
--- services/redux/rootReducer.js
+++ services/redux/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import bookings, { name as bookingsName } from '@hello-poland/commons/redux/bookings';
+import categories, { name as categoriesName } from '@hello-poland/commons/redux/categories';
 import partners, { name as partnersName } from 'redux/partners';
 import profile, { name as profileName } from 'redux/profile';
 import sightEvents, { name as sightEventsName } from '@hello-poland/commons/redux/sightEvents';
@@ -7,6 +8,7 @@ import ticketDefinitions, { name as ticketDefinitionsName } from '@hello-poland/
 
 export default combineReducers({
   [bookingsName]: bookings(),
+  [categoriesName]: categories(),
   [partnersName]: partners(),
   [profileName]: profile(),
   [sightEventsName]: sightEvents(),
